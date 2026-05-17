0da14727b96daa7bb8de01be209db2355deccdc0
CS041 Faatih <22oo1cso41faatih@gmail.com>
2026-05-17 07:33:03 +0530

    feat(ui): added 404 dark lottie animation

--- components/NotFoundAnimation.tsx
+++ components/NotFoundAnimation.tsx
@@ -8,6 +8,7 @@ export default function NotFoundAnimation() {
       <div className="w-48 h-48 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80">
         <DotLottieReact
           src="https://lottie.host/a78410a6-e8ed-443c-9566-14734cc1d2c9/CbfCBwn8X5.lottie"
+          // dark lottie animation     // src="https://lottie.host/4940dc8f-bea3-4e77-972a-204ed3649312/JoSDol1xyk.lottie"
           loop
           autoplay
         />
